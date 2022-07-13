8bb7323eab92471a1191f87c7a9edf19a9c5c411
Victor <fisheve@mx.lakeforest.edu>
2022-07-12 19:23:52 -0500

    Container Added to Navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,19 @@
 <template>
     <div>
         <b-navbar toggleable="lg" variant="light" type="light">
-            <b-navbar-brand href="#">Victor's Library</b-navbar-brand>
+            <!-- The Media Query in default.vue was necessary to add a container here. -->
+            <div  class="container">
+                <b-navbar-brand href="#">Victor's Library</b-navbar-brand>
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>         
-                <b-navbar-nav class="ml-auto">
-                    <b-nav-item href="https://bandcamp.com/contact?b=1631569890" target="_blank">Contact</b-nav-item>
-                    <b-nav-item href="http://songs.victorslibrary.com" target="_blank">Old Version of This Website</b-nav-item>
-                </b-navbar-nav> 
-            </b-collapse>
+                <b-collapse id="nav-collapse" is-nav>         
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item href="https://bandcamp.com/contact?b=1631569890" target="_blank">Contact</b-nav-item>
+                        <b-nav-item href="http://songs.victorslibrary.com" target="_blank">Old Version of This Website</b-nav-item>
+                    </b-navbar-nav> 
+                </b-collapse>
+            </div>
         </b-navbar>
     </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,3 +6,17 @@
     </b-container>
   </div>
 </template>
+
+<style>
+  /* This Media Query was needed to add a container to the menu in Navbar.vue  */
+  .container {
+    padding: 0
+  }
+  @media only screen and (max-width: 575px) {
+    .container {
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+  }
+
+</style>
