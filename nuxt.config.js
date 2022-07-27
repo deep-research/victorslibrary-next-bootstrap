015f1313ef646f6806c4d199f2e7884d97f60191
Victor <fisheve@mx.lakeforest.edu>
2022-07-27 08:01:02 -0500

    Testing Favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
 
       // https://favicon.io/emoji-favicons/books
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
