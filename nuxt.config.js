abbdd7df0d7aadf123af6c456135710b516ee173
Victor <fisheve@mx.lakeforest.edu>
2022-07-27 08:14:20 -0500

    Problem with apple touch icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,9 +25,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
 
       // https://favicon.io/emoji-favicons/books
-      // { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      // { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      // { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       // { rel: 'manifest', href: '/site.webmanifest' }
     ]
   },
