4360a8aaab398598b243497ba54ec83d2ff62ccd
Victor <fisheve@mx.lakeforest.edu>
2022-07-27 07:52:03 -0500

    Updating Favicon to Books

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+
+      // https://favicon.io/emoji-favicons/books
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon_io/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon_io/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon_io/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicon_io/site.webmanifest' }
     ]
   },
 
