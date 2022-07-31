3647f98d33164fafd65326902b6745ea6499c6ba
Victor <fisheve@mx.lakeforest.edu>
2022-07-31 03:06:00 -0500

    Pug, TS and SCSS Working

---
+++ src/routes/preprocess.svelte
@@ -0,0 +1,13 @@
+<template lang="pug">
+  h1 {name}
+</template>
+
+<script lang="ts">
+  export let name: string = 'world';
+</script>
+
+<style lang="scss">
+  h1 {
+    color: $primary-color;
+  }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,33 @@
 import adapter from "@sveltejs/adapter-static";
 import preprocess from 'svelte-preprocess';
+import autoprefixer from 'autoprefixer'
 
 const dev = process.env.NODE_ENV === 'development';
 
-// How to Deploy SvelteKit Apps to Github Pages
+// Guides Used:
+
+// 1. How to Deploy SvelteKit Apps to Github Pages
 // https://sveltesaas.com/articles/sveltekit-github-pages-guide/
+
+// 2. Svelte-Preprocess Getting Started Guide
+// https://github.com/sveltejs/svelte-preprocess/blob/main/docs/getting-started.md
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: preprocess({
+		sourceMap: dev,
+		scss: {
+			// We can use a path relative to the root because
+			// svelte-preprocess automatically adds it to `includePaths`
+			// if none is defined.
+				prependData: `@import 'src/styles/variables.scss';`
+			},
+		postcss: {
+			plugins: [autoprefixer()]
+		}		
+	}),
 
     kit: {
         adapter: adapter({
