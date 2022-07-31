f9b0e76c65427f9acd27443fc8f7986fcab62088
Victor <fisheve@mx.lakeforest.edu>
2022-07-31 00:36:50 -0500

    Favicon Updated to Books

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to Victor's Library</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Visit <a href="https://songs.victorslibrary.com">songs.victorslibrary.com</a> to see the older version.</p>
