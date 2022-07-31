3ba5926e3639f1f902684f128868544116ea5b07
Victor <fisheve@mx.lakeforest.edu>
2022-07-31 02:40:08 -0500

    Comment Added With Deployment Guide

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import preprocess from 'svelte-preprocess';
 
 const dev = process.env.NODE_ENV === 'development';
 
+// How to Deploy SvelteKit Apps to Github Pages
+// https://sveltesaas.com/articles/sveltekit-github-pages-guide/
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
