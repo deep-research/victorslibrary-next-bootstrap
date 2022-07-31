20f58dbea9a55e460ce126940ee4c19d5c2b3107
Victor <fisheve@mx.lakeforest.edu>
2022-07-31 02:34:46 -0500

    Sveltekit Reinstalled

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,15 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
-
-/** @type {import(""@sveltejs/kit").Config} */
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-    kit: {
-        adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "" : "",
-        },
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
-    }
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
