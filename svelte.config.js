8c75f75299043504e4b1d27c8daf58a5f14740ae
Victor <fisheve@mx.lakeforest.edu>
2022-07-30 23:21:45 -0500

    Sveltekit Deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,23 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static";
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import(""@sveltejs/kit").Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+        }),
+        paths: {
+            // change below to your repo name
+            base: dev ? "" : "",
+        },
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
+    }
 };
 
 export default config;
