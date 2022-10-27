e725eee36fd133e15d14b249783d797047a1c849
Victor <fisheve@mx.lakeforest.edu>
2022-10-27 09:40:58 -0500

    Nuxt Content Working

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  typescript: {
+    shim: false
+  },
 
+  modules: [
+    '@nuxt/content'
+  ],
+  
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
 })
