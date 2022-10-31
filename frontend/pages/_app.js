ca334ec12041fb63088d0a56a78280514d3897d2
Victor <fisheve@mx.lakeforest.edu>
2022-10-30 20:43:14 -0500

    Bootstrap Guide Saved

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
+// https://www.kindacode.com/article/how-to-correctly-use-bootstrap-5-in-next-js/#Installing
+import 'bootstrap/dist/css/bootstrap.css'
 import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
