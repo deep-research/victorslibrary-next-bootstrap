f65737a6a85a1cd6ce415ce27fd2c78b526bfbcf
Victor <fisheve@mx.lakeforest.edu>
2022-10-30 23:28:34 -0500

    remove workflow

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: {
-    unoptimized: true,
-  }
 }
 
 module.exports = nextConfig
