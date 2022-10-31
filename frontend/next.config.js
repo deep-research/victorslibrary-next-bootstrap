bac4a5e21296307f811e9b2d7126494ef7783430
Victor <fisheve@mx.lakeforest.edu>
2022-10-31 14:53:54 -0500

    Without unoptimized flag

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    unoptimized: true,
+    // unoptimized: true,
   },
   withOptimizedImages: {
     handleImages: [
