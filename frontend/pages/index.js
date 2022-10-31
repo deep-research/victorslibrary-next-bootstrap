a807edd7b41fcf31bdf8bd61019679037fc5f68f
Victor <fisheve@mx.lakeforest.edu>
2022-10-31 16:04:44 -0500

    Currently Stable

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,8 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import ExportedImage from "next-image-export-optimizer";
-
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -56,14 +54,6 @@ export default function Home() {
         </div>
       </main>
 
-      <ExportedImage
-        src="images/night-forest.jpg"
-        alt="Large Image"
-        width={500}
-        height={500}
-        useWebp={process.env.nextImageExportOptimizer_storePicturesInWEBP}
-      />;
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -72,7 +62,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
