be9a358fbb7d5936576242b05b0cceebec751b0b
Victor <fisheve@mx.lakeforest.edu>
2022-11-01 13:11:36 -0500

    Config File Plugins Bug Fully Evaluated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,29 +1,34 @@
-// @type {import('next').NextConfig}
-
-//  const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-  
-//   images: {
-//     unoptimized: true,
-//   }
-// }
-
 // module.exports = nextConfig
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
+const { isModuleInstalled } = require('next-optimized-images/lib/loaders')
 
-const nextConfig = {
+module.exports = optimizedImages({
   images: {
     disableStaticImages: true,
-  } 
-}
+  }   
+})
 
-module.exports = withPlugins([
-  [optimizedImages, {
-    /* config for next-optimized-images */
-  }],
+// ----
 
-  // your other plugins here
+// related issues:
+// next-compose-plugins issue: https://github.com/vercel/next.js/issues/39161#issue-comment-box
+// syntax without next-compose-plugins: https://stackoverflow.com/questions/65604469/how-to-combine-several-plugins-inside-next-config-js-file
+// original commit: https://github.com/deep-research/victorslibrary-metadata/commit/4779ef6b52549a02726fbefd0099180de98a49cc
 
-], nextConfig);
+// syntax with next-compose-plugins:
+// 
+// const nextConfig = {
+//   images: {
+//     disableStaticImages: true,
+//   } 
+// }
+
+// module.exports = withPlugins([
+//   [optimizedImages, {
+//     /* config for next-optimized-images */
+//   }],
+
+//   // your other plugins here
+
+// ], nextConfig);
