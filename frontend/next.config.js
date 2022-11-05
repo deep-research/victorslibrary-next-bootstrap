35c0a7ea96cf9ca9d0be719bdc0182fd29297e2a
Victor <fisheve@mx.lakeforest.edu>
2022-11-05 01:41:25 -0500

    Trying to remove next-compose-plugins

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,45 +1,19 @@
-// module.exports = nextConfig
-const withPlugins = require('next-compose-plugins');
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const withOptimizedImages = require('next-optimized-images');
 
-// module.exports = withOptimizedImages({
-//   images: {
-//     disableStaticImages: true,
-//   }
-// })
-
-module.exports = withPlugins(
-  [
-    [withOptimizedImages]
-  ],
-  {
-    images: {
-      disableStaticImages: true,
-    }
+const nextConfig = {
+  images: {
+    disableStaticImages: true,
   }
-)
+}
 
-// ----
+module.exports = () => {
+ const plugins = [
+  withOptimizedImages
+]
 
-// related issues:
-// next-compose-plugins issue: https://github.com/vercel/next.js/issues/39161#issue-comment-box
-// syntax without next-compose-plugins: https://stackoverflow.com/questions/65604469/how-to-combine-several-plugins-inside-next-config-js-file
-// original commit: https://github.com/deep-research/victorslibrary-metadata/commit/4779ef6b52549a02726fbefd0099180de98a49cc
-// build issue in prod, downgrade packages: https://github.com/cyrilwanner/next-optimized-images/issues/270
-
-// syntax with next-compose-plugins:
-// 
-// const nextConfig = {
-//   images: {
-//     disableStaticImages: true,
-//   } 
-// }
-
-// module.exports = withPlugins([
-//   [optimizedImages, {
-//     /* config for next-optimized-images */
-//   }],
-
-//   // your other plugins here
-
-// ], nextConfig);
+ return plugins.reduce((config, plugin) => plugin(config), nextConfig);
+}
